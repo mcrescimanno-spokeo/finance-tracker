4fa5fc1300f76f96303fc86fd592feaa1e043732
Matthew Crescimanno <mcrescimanno@spokeo.com>
2022-05-27 15:08:25 -0700

    feat: update views

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,8 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence: true
 
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
@@ -6,7 +10,14 @@ class Stock < ApplicationRecord
       # secret_token: Rails.application.credentials.iex_cloud.secret_access_key,
       endpoint: 'https://sandbox.iexapis.com/v1'
     )
+    begin
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+    rescue => exception
+      return nil
+    end
+  end
 
-    client.price(ticker_symbol)
+  def self.check_db(ticker_symbol)
+    where(ticker: ticker_symbol).first
   end
 end
--- db/migrate/20191223132113_create_stocks.rb
+++ db/migrate/20191223132113_create_stocks.rb
@@ -1,4 +1,4 @@
-class CreateStocks < ActiveRecord::Migration[6.1]
+class CreateStocks < ActiveRecord::Migration[6.0]
   def change
     create_table :stocks do |t|
       t.string :ticker
